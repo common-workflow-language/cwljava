a265fa3743a6e6f77ed24727cd3a8412f37b8c91
Michael R. Crusoe <1330696+mr-c@users.noreply.github.com>
2021-02-02 10:21:36 +0100

    add test for Workflows being the correct class

---
+++ src/test/java/org/w3id/cwl/cwl1_2/utils/WorkflowClassTest.java
@@ -0,0 +1,36 @@
+package org.w3id.cwl.cwl1_2.utils;
+
+import org.w3id.cwl.cwl1_2.Process;
+import org.w3id.cwl.cwl1_2.SchemaDefRequirement;
+import org.w3id.cwl.cwl1_2.CWLVersion;
+import org.w3id.cwl.cwl1_2.InlineJavascriptRequirement;
+import org.w3id.cwl.cwl1_2.InputRecordSchema;
+
+import java.net.URISyntaxException;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class WorkflowClassTest {
+	Process doc;
+	
+	public WorkflowClassTest() throws URISyntaxException {
+		super();
+		this.doc = (Process) RootLoader
+				.loadDocument(java.nio.file.Paths.get(getClass().getResource("valid_count-lines1-wf.cwl").toURI()));
+	}
+
+	@Test
+	public void className() {
+		Assert.assertEquals("WorkflowImpl", doc.getClass().getSimpleName());
+	}
+
+	@Test
+	public void version() {
+		java.util.Optional<CWLVersion> version = doc.getCwlVersion();
+		Assert.assertTrue(version.isPresent());
+		Assert.assertEquals(CWLVersion.V1_2, version.get());
+	}
+
+}
