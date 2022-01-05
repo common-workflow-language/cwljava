29857c5fe7b3e72dc875dbcf2bc9ccebbe5aec0b
Michael R. Crusoe <michael.crusoe@gmail.com>
2022-01-05 16:50:54 +0100

    scope: chop off the correct end of the fragment

---
+++ src/test/java/org/w3id/cwl/cwl1_2/utils/PackedWorkflowClassTest.java
@@ -0,0 +1,46 @@
+package org.w3id.cwl.cwl1_2.utils;
+
+import org.w3id.cwl.cwl1_2.Process;
+import org.w3id.cwl.cwl1_2.SchemaDefRequirement;
+import org.w3id.cwl.cwl1_2.Workflow;
+import org.w3id.cwl.cwl1_2.WorkflowStep;
+import org.w3id.cwl.cwl1_2.WorkflowStepInput;
+import org.w3id.cwl.cwl1_2.CWLVersion;
+import org.w3id.cwl.cwl1_2.InlineJavascriptRequirement;
+import org.w3id.cwl.cwl1_2.InputRecordSchema;
+
+import java.net.URISyntaxException;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class PackedWorkflowClassTest {
+	List<Process> doc;
+	
+	@SuppressWarnings("unchecked")
+	public PackedWorkflowClassTest() throws URISyntaxException {
+		super();
+		this.doc = (List<Process>) RootLoader
+				.loadDocument(Paths.get(getClass().getResource("valid_scatter-wf4.cwl").toURI()));
+	}
+
+	@Test
+	public void className() {
+		Workflow workflow = (Workflow) doc.get(1);
+		Assert.assertEquals("WorkflowImpl", workflow.getClass().getSimpleName());
+	}
+	
+	@Test
+	public void workflowStepInputSources() {
+		Workflow workflow = (Workflow) doc.get(1);
+		String workflow_id = workflow.getId().get();
+		WorkflowStep step1 = (WorkflowStep) workflow.getSteps().get(0);
+		List<Object> inputs = step1.getIn();
+		WorkflowStepInput step1_input1 = (WorkflowStepInput) inputs.get(0);
+		Assert.assertEquals(workflow_id + "/step1/echo_in1", step1_input1.getId().get());
+		Assert.assertEquals(workflow_id + "/inp1", step1_input1.getSource());
+	}
+
+}
